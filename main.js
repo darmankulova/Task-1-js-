1d865cd4997d258c01d355e8dde9dd79d07d02d7
darmankulova <imetoffa1@gmail.com>
2023-10-15 16:13:42 +0600

    hw js1

---
+++ main.js
@@ -0,0 +1,16 @@
+//Task 1: Основы переменных
+//Создайте три переменные: имя, возраст и город. Присвойте им значения вашего имени, вашего возраста и вашего города. Выведите их значения в консоль.
+let name = 'Nurperi';
+let age = 26;
+let city = 'Osh';
+console.log(name,age,city);
+
+//Task 2
+let fav_color = 'vilvet';
+let fav_number = 7;
+console.log(typeof fav_color, typeof fav_number);
+
+//Task 3
+let a = 'Hello';
+let b = 'World';
+console.log(a+' '+b);
